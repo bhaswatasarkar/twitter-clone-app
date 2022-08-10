dc2b1156b4114eeb1536375dd5ee9914a1e8c346
Bhaswata Sarkar <bhaswatasarkar1@gmail.com>
2022-08-11 00:29:41 +0530

    Error message added in newslist

--- src/components/Rightwidgetbar/NewsList.js
+++ src/components/Rightwidgetbar/NewsList.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 
 
 
-function NewsList() {
+function NewsList({searchString}) {
+
 const [articles, setArticles] = useState([])
 
 // 'https://newsapi.org/v2/everything?q=sports&from=2022-07-20&sortBy=popularity&apiKey=a3d4aab744ae4ceea87848b0c70454f6'
@@ -12,24 +13,47 @@ const [articles, setArticles] = useState([])
 // 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=everything&api-key=Ck69Npn9eeRCRZ8lXYvjv6zKXVTy2lAX'
 
 useEffect(() => {
-    var myurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=everything&api-key=Ck69Npn9eeRCRZ8lXYvjv6zKXVTy2lAX'
+    var myurl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchString}&api-key=Ck69Npn9eeRCRZ8lXYvjv6zKXVTy2lAX`
+    
     const getArticles = async () => {
-        const response = await axios.get(`${myurl}`)
-        console.log(response);
-        setArticles(response.data.response.docs)
+        
+            const response = await axios.get(`${myurl}`).catch((err=>{
+                document.getElementById('news-components-error').innerHTML="Failed to coonect to the server!"
+            }))
+
+            console.log(response);
+            try{
+                document.getElementById('news-components-error').innerHTML="";
+                setArticles(response.data.response.docs)
+            }
+            catch(e){
+                document.getElementById('news-components-error').innerHTML="Too many requests please refresh or type again after a few time!"
+            }
+        
     }
     getArticles();
-},[])
+},[searchString])
 
 function NewsItemConfigure({index}){
     index = parseInt(index)
-    return (<NewsItem
-        title={articles[index].headline.main}
-        author={articles[index].byline.original}
-        sourcename={articles[index].source}
-        urlToImage={"http://www.nytimes.com/"+articles[index].multimedia[0].url}
-        sectionName={articles[index].section_name}
-    />)
+
+    if(articles[index].multimedia.length>=1){
+        return (<NewsItem
+            title={articles[index].headline.main}
+            author={articles[index].byline.original}
+            sourcename={articles[index].source}
+            urlToImage={"http://www.nytimes.com/"+articles[index].multimedia[0].url}
+            sectionName={articles[index].section_name}
+        />)
+    }
+    else{
+        return (<NewsItem
+            title={articles[index].headline.main}
+            author={articles[index].byline.original}
+            sourcename={articles[index].source}
+            sectionName={articles[index].section_name}
+        />)
+    }
 }
 
 if(articles.length>5){
@@ -54,6 +78,7 @@ else{
   return(<></>)
 }
 }
+
 
 
 export default NewsList
--- src/components/Rightwidgetbar/Rightwidgetbar.js
+++ src/components/Rightwidgetbar/Rightwidgetbar.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import './Rightwidgetbar.css'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import NewsList from './NewsList';
 function Rightwidgetbar() {
+  const[searchString,setSearchString] = useState("");
+  useEffect(() => {
+    document.title = searchString
+})
+
   return (
     <div className='rightwidgetbar'>
       <div className='widget-input'>
         <SearchRoundedIcon className='widget-search-icon'/>
-        <input placeholder='Search Twitter' className='widget-search-text'></input>
+        <input placeholder='Search Twitter' value={searchString} className='widget-search-text' onChange={e=>setSearchString(e.target.value)}></input>
       </div>
 
       <div className='news-container'>
         <div className='news-container-header'>What's happening</div>
         <div className='news-components'>
-          <NewsList/>
-          
+          <div id='news-components-error'></div>
+          <NewsList id="news-list" searchString={searchString}/>
         </div>
         <div className='news-container-footer'>Show more</div>
       </div>
